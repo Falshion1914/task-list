cee91b764fb9cbd9b30f9748147a08d7c0e2aac4
Strong2045 <sergeyadmit@gmail.com>
2020-06-19 06:24:45 +0300

    add package

---
+++ dist/bundle.js
@@ -0,0 +1,100 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./index.js":
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("let elem = document.createElement(\"input\");\nelem.onkeydown = events;\ndocument.body.appendChild(elem);\nelem = document.createElement(\"button\");\nelem.innerHTML = \"click\";\ndocument.body.appendChild(elem);\nconst parent = document.getElementById(\"container\");\nconst list = document.createElement(\"ul\");\nparent.appendChild(list);\nfunction srt(name) {\n  document.querySelector(\"ul\").innerHTML = \"\";\n  if (localStorage.tasks !== undefined) {\n    const arr = JSON.parse(localStorage.tasks).data;\n    for (let i = 0; i < arr.length; i++) {\n      const temp = document.createElement(\"li\");\n      temp.innerHTML = arr[i];\n      list.appendChild(temp);\n    }\n  }\n}\nsrt();\nfunction events(e) {\n  if (e.which === 13 || e.which === 1) {\n    const val = document.getElementsByTagName(\"input\")[0].value;\n    vals = val.replace(/\\s+/g, \"\");\n    if (val !== undefined && vals !== \"\") {\n      if (localStorage.tasks !== undefined) {\n        const obj = JSON.parse(localStorage.tasks);\n        obj.data.push(val);\n        obj.data.sort();\n        localStorage.tasks = JSON.stringify(obj);\n      } else {\n        localStorage.tasks = JSON.stringify({ data: [val] });\n      }\n      const el = document.createElement(\"li\");\n      el.innerHTML = val;\n      list.appendChild(el);\n      srt();\n    }\n    document.getElementsByTagName(\"input\")[0].value = \"\";\n  }\n}\n\nelem.onclick = events;\n\n\n//# sourceURL=webpack:///./index.js?");
+
+/***/ })
+
+/******/ });
--- index.js
+++ index.js
@@ -1,45 +1,44 @@
-let elem = document.createElement('input');
+let elem = document.createElement("input");
 elem.onkeydown = events;
 document.body.appendChild(elem);
-elem = document.createElement('button');
-elem.innerHTML = 'click';
+elem = document.createElement("button");
+elem.innerHTML = "click";
 document.body.appendChild(elem);
-let parent = document.getElementById("container");
-let list = document.createElement('ul');
+const parent = document.getElementById("container");
+const list = document.createElement("ul");
 parent.appendChild(list);
-function srt (name) {
-    document.querySelector('ul').innerHTML = '';
-    if (localStorage.tasks != undefined) {
-        let arr = JSON.parse(localStorage.tasks).data;
-        for (let i = 0; i < arr.length; i++) {
-            let temp = document.createElement('li');
-            temp.innerHTML = arr[i];
-            list.appendChild(temp);
-        }
+function srt(name) {
+  document.querySelector("ul").innerHTML = "";
+  if (localStorage.tasks !== undefined) {
+    const arr = JSON.parse(localStorage.tasks).data;
+    for (let i = 0; i < arr.length; i++) {
+      const temp = document.createElement("li");
+      temp.innerHTML = arr[i];
+      list.appendChild(temp);
     }
+  }
 }
 srt();
 function events(e) {
-    if (e.which == 13 || e.which == 1) {
-        let val = document.getElementsByTagName("input")[0].value;
-        vals = val.replace(/\s+/g,'');
-        if (val != undefined && vals != "") {
-            if (localStorage.tasks != undefined) {
-                let obj = JSON.parse(localStorage.tasks);
-                obj.data.push(val);
-                obj.data.sort();
-                console.log(obj);
-                localStorage.tasks = JSON.stringify(obj);
-            } else {
-                localStorage.tasks = JSON.stringify({data: [val]});
-            }
-            let el = document.createElement('li');
-            el.innerHTML = val;
-            list.appendChild(el);
-            srt();
-        }
-        document.getElementsByTagName("input")[0].value = '';
+  if (e.which === 13 || e.which === 1) {
+    const val = document.getElementsByTagName("input")[0].value;
+    vals = val.replace(/\s+/g, "");
+    if (val !== undefined && vals !== "") {
+      if (localStorage.tasks !== undefined) {
+        const obj = JSON.parse(localStorage.tasks);
+        obj.data.push(val);
+        obj.data.sort();
+        localStorage.tasks = JSON.stringify(obj);
+      } else {
+        localStorage.tasks = JSON.stringify({ data: [val] });
+      }
+      const el = document.createElement("li");
+      el.innerHTML = val;
+      list.appendChild(el);
+      srt();
     }
+    document.getElementsByTagName("input")[0].value = "";
+  }
 }
 
 elem.onclick = events;
